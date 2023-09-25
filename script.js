764c1051f772ca0cc2bd7268849bd63e3d3cdafe
Pathompong Thitithan <the.midnight.k.0173@gmail.com>
2023-09-25 22:53:05 +0700

    make fullscreen responsive

--- script.js
+++ script.js
@@ -1,24 +1,24 @@
-function getMeatIng(){
-    const meatProc = ["","สับ","กรอบ","ทอด","ต้ม","ย่าง","อบ"];
+function getMeatIng() {
+    const meatProc = ["", "สับ", "กรอบ", "ทอด", "ต้ม", "ย่าง", "อบ"];
     let proc = meatProc[Math.floor(Math.random() * meatProc.length)];
-    const meatIng = [ 
-        "หมู"+proc, "ไก่"+proc, "เนื้อ"+proc, "ปลา"+proc, 
-        "ทะเล","กุ้ง","ปลาหมึก","ปู","ปูอัด","ใส้กรอก","ไข่เยี่ยวม้า"
+    const meatIng = [
+        "หมู" + proc, "ไก่" + proc, "เนื้อ" + proc, "ปลา" + proc,
+        "ทะเล", "กุ้ง", "ปลาหมึก", "ปู", "ปูอัด", "ใส้กรอก", "ไข่เยี่ยวม้า"
     ];
     return meatIng[Math.floor(Math.random() * meatIng.length)];
 }
-function getVegIng(){
-    const vegIng = ["คะน้า","ผักบุ้ง","ผักรวม","แครอท"]
+function getVegIng() {
+    const vegIng = ["คะน้า", "ผักบุ้ง", "ผักรวม", "แครอท"];
     return vegIng[Math.floor(Math.random() * vegIng.length)];
 }
-function getTopping(){
-    const topping = ["","ไข่เจียว","ไข่ดาว","ไข่ข้น"]
+function getTopping() {
+    const topping = ["", "ไข่เจียว", "ไข่ดาว", "ไข่ข้น"];
     return topping[Math.floor(Math.random() * topping.length)];
 }
 function getMenu() {
     let meat = getMeatIng();
     let veg = getVegIng();
-    let top = getTopping();let menus = [
+    let top = getTopping(); let menus = [
         `ข้าวผัด${meat}${top}`,
         `ข้าวผัด${meat}${veg}${top}`,
         `${meat}ทอดกระเทียม${top}`,
@@ -30,7 +30,7 @@ function getMenu() {
         `${veg}${meat}`,
         `${meat}ผัดเผ็ด${top}`,
         `${meat}ผัดพริกแกง${top}`
-    ]
+    ];
     return menus[Math.floor(Math.random() * menus.length)];
 }
 
@@ -38,7 +38,7 @@ let getMenuBtn = document.getElementById("getMenuBtn");
 let menuTitle = document.getElementById("title");
 let subtitle = document.getElementById("subtitle");
 let randCount = 0;
-getMenuBtn.addEventListener("click", function(){
+getMenuBtn.addEventListener("click", function () {
     menuTitle.innerHTML = getMenu();
     randCount++;
     if (randCount >= 10) {
