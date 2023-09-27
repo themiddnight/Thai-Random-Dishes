7931973333c3c54343dc8939358dd38695db1909
Pathompong Thitithan <the.midnight.k.0173@gmail.com>
2023-09-28 03:00:34 +0700

    add custom settings

--- script.js
+++ script.js
@@ -1,41 +1,177 @@
-function getMeatIng() {
-    const meatProc = ["", "สับ", "กรอบ", "ทอด", "ต้ม", "ย่าง", "ย่างเกลือ", "อบ", "เผา"];
-    let proc = meatProc[Math.floor(Math.random() * meatProc.length)];
-    const meatIng = [
-        "หมู" + proc, "ไก่" + proc, "เนื้อ" + proc,
-        "ปลา", "ทะเล", "กุ้ง", "ปลาหมึก", "ปู", "ปูอัด", "ใส้กรอก", "ไข่เยี่ยวม้า"
-    ];
-    return meatIng[Math.floor(Math.random() * meatIng.length)];
+class Menus {
+    constructor() {
+        this.menuExc = [];
+        this.meatProcExc = [];
+        this.meatExc = [];
+        this.vegExc = [];
+        this.topExc = [];
+
+        this.meatProcList = ["", "สับ", "กรอบ", "ทอด", "ต้ม",
+            "ย่าง", "ย่างเกลือ", "อบ", "เผา"];
+        const proc = this.getRandProc()
+        this.meatListObj = {
+            "หมู": "หมู" + proc,
+            "ไก่": "ไก่" + proc,
+            "เนื้อ": "เนื้อ" + proc,
+            "ปลา": "ปลา" + proc,
+            "กุ้ง": "กุ้ง" + proc,
+            "ทะเล": "ทะเล",
+            "ปลาหมึก": "ปลาหมึก",
+            "ปู": "ปู",
+            "ปูอัด": "ปูอัด",
+            "ใส้กรอก": "ใส้กรอก",
+            "ไข่เยี่ยวม้า": "ไข่เยี่ยวม้า"
+        };
+        this.meatList = Object.keys(this.meatListObj);
+        this.vegList = ["", "คะน้า", "ผักบุ้ง", "ผักรวม", "แครอท",
+            "กะหล่ำปลี", "บล็อคโคลี่", "ผักกาด"];
+        this.topList = ["", "ไข่เจียว", "ไข่ดาว", "ไข่ข้น", "ไข่เค็ม"];
+        this.menuList = [
+            "ข้าวผัด",
+            "ทอดกระเทียม",
+            "ทอดน้ำปลา",
+            "ผัดน้ำมันหอย",
+            "คั่วพริกเกลือ",
+            "กระเพรา",
+            "ข้าวไข่",
+            "ผัดเผ็ด",
+            "ผัดพริกแกง",
+            "ผัดผงกะหรี่"
+        ]
+    }
+
+    randomArray(array) {
+        return array[Math.floor(Math.random() * array.length)];
+    }
+
+    randomObj(obj, exclude) {
+        const keys = Object.keys(obj).filter(key => !exclude.includes(key));
+        const randomIndex = Math.floor(Math.random() * keys.length);
+        const randomKey = keys[randomIndex];
+        return obj[randomKey];
+    }
+
+    getRandProc() {
+        let proc;
+        do {
+            proc = this.randomArray(this.meatProcList);
+        } while (this.meatProcExc.includes(proc));
+        return proc;
+    }
+
+    getRandIngredient() {
+        let meat, veg, top;
+        meat = this.randomObj(this.meatListObj, this.meatExc);
+        do {
+            veg = this.randomArray(this.vegList);
+        } while (this.vegExc.includes(veg));
+        do {
+            top = this.randomArray(this.topList);
+        } while (this.topExc.includes(top));
+        return [meat, veg, top];
+    }
+
+    getMenu() {
+        const [, veg, top] = this.getRandIngredient();
+        // re-random proc
+        const proc = this.getRandProc()
+        this.meatListObj = {
+            "หมู": "หมู" + proc,
+            "ไก่": "ไก่" + proc,
+            "เนื้อ": "เนื้อ" + proc,
+            "ปลา": "ปลา" + proc,
+            "กุ้ง": "กุ้ง" + proc,
+            "ทะเล": "ทะเล",
+            "ปลาหมึก": "ปลาหมึก",
+            "ปู": "ปู",
+            "ปูอัด": "ปูอัด",
+            "ใส้กรอก": "ใส้กรอก",
+            "ไข่เยี่ยวม้า": "ไข่เยี่ยวม้า"
+        };
+        this.meatList = Object.keys(this.meatListObj);
+        const meat = this.randomObj(this.meatListObj, this.meatExc);
+
+        this.menusObj = {
+            "ข้าวผัด": `ข้าวผัด${meat}${veg}${top}`,
+            "ทอดกระเทียม": `${meat}ทอดกระเทียม${top}`,
+            "ทอดน้ำปลา": `${meat}ทอดน้ำปลา${top}`,
+            "ผัดน้ำมันหอย": `ผัด${veg}${meat}น้ำมันหอย${top}`,
+            "คั่วพริกเกลือ": `${meat}คั่วพริกเกลือ${top}`,
+            "กระเพรา": `กระเพรา${meat}${top}`,
+            "ข้าวไข่": `ข้าว${top}${meat}${veg}`,
+            "ผัดเผ็ด": `${meat}ผัดเผ็ด${top}`,
+            "ผัดพริกแกง": `${meat}ผัดพริกแกง${top}`,
+            "ผัดผงกะหรี่": `${meat}ผัดผงกะหรี่${top}`
+        };
+        return this.randomObj(this.menusObj, this.menuExc);
+    }
 }
-function getVegIng() {
-    const vegIng = ["คะน้า", "ผักบุ้ง", "ผักรวม", "แครอท", "กะหล่ำปลี", "บล็อคโคลี่", "ผักกาด"];
-    return vegIng[Math.floor(Math.random() * vegIng.length)];
+
+const menus = new Menus();
+
+//////////////////////// generate setting menu in html ////////////////////////
+
+function generateHtmlList(list, listDivId) {
+    if (list.includes("")) {
+        list.splice(list.indexOf(""), 1);
+    }
+    const listDiv = document.getElementById(listDivId);
+    list.forEach(menu => {
+        const label = document.createElement("label");
+        label.classList.add("check-container");
+        label.innerHTML = `${menu}<input type="checkbox" value="${menu}" checked>
+                            <span class="checkmark"></span>`;
+        listDiv.appendChild(label);
+    });
+    listDiv.querySelectorAll("input[type=checkbox]")
+    .forEach(checkbox => {
+        checkbox.addEventListener("click", addExcludeItems);
+    });
 }
-function getTopping() {
-    const topping = ["", "ไข่เจียว", "ไข่ดาว", "ไข่ข้น", "ไข่ต้ม", "ไข่เค็ม", "ไข่เยี่ยวม้า"];
-    return topping[Math.floor(Math.random() * topping.length)];
+
+generateHtmlList(menus.menuList, "menuList");
+generateHtmlList(menus.meatList, "meatList");
+generateHtmlList(menus.meatProcList, "meatProcList");
+generateHtmlList(menus.vegList, "vegList");
+generateHtmlList(menus.topList, "topList");
+
+// get all of checkbox in each ListDiv
+const menuListCheckbox = document.getElementById("menuList")
+.querySelectorAll("input[type=checkbox]");
+const meatListCheckbox = document.getElementById("meatList")
+.querySelectorAll("input[type=checkbox]");
+const meatProcListCheckbox = document.getElementById("meatProcList")
+.querySelectorAll("input[type=checkbox]");
+const vegListCheckbox = document.getElementById("vegList")
+.querySelectorAll("input[type=checkbox]");
+const topListCheckbox = document.getElementById("topList")
+.querySelectorAll("input[type=checkbox]");
+
+// get all of the value of checkboxs that get checked in ListDiv
+function getCheckedMenu(checkboxes) {
+    const unchecked = [];
+    checkboxes.forEach(checkbox => {
+        if (!checkbox.checked) {
+            unchecked.push(checkbox.value);
+        }
+    });
+    return unchecked;
 }
-function getMenu() {
-    let meat = getMeatIng();
-    let veg = getVegIng();
-    let top = getTopping(); 
-    let menus = [
-        `ข้าวผัด${meat}${top}`,
-        `ข้าวผัด${meat}${veg}${top}`,
-        `${meat}ทอดกระเทียม${top}`,
-        `${meat}ทอดน้ำปลา${top}`,
-        `ผัด${veg}${meat}น้ำมันหอย${top}`,
-        `${meat}คั่วพริกเกลือ${top}`,
-        `กระเพรา${meat}${top}`,
-        `ข้าว${top}${meat}`,
-        `ข้าว${top}${meat}${veg}`,
-        `${veg}${meat}น้ำมันหอย`,
-        `${meat}ผัดเผ็ด${top}`,
-        `${meat}ผัดพริกแกง${top}`,
-        `${meat}ผัดผงกะหรี่${top}`,
-    ];
-    return menus[Math.floor(Math.random() * menus.length)];
+
+function addExcludeItems() {
+    menus.menuExc = [];
+    menus.meatProcExc = [];
+    menus.meatExc = [];
+    menus.vegExc = [];
+    menus.topExc = [];
+    menus.menuExc = getCheckedMenu(menuListCheckbox);
+    menus.meatProcExc = getCheckedMenu(meatProcListCheckbox);
+    menus.meatExc = getCheckedMenu(meatListCheckbox);
+    menus.vegExc = getCheckedMenu(vegListCheckbox);
+    menus.topExc = getCheckedMenu(topListCheckbox);
 }
+
+//////////////////////// do random menu ////////////////////////
 
 function setMenuTx() {
     return new Promise((resolve) => {
@@ -43,40 +179,38 @@ function setMenuTx() {
         let interval = 70;
         let menuTitle = document.getElementById("title");
         function executeLoop() {
-            menuTitle.innerHTML = getMenu();
+            menuTitle.innerHTML = menus.getMenu();
             count++;
             if (count >= 15) {
                 resolve();
-            }
-            else {
+            } else {
                 interval *= 1.1;
                 setTimeout(executeLoop, interval);
             }
-        }
-        executeLoop();
+        } executeLoop();
     });
 }
 
 function popMenuTitleCon() {
-    let menuTitleCon = document.getElementById("title-con");
-        if (menuTitleCon.classList.contains("pop")) {
-            menuTitleCon.classList.remove("pop");
-        }
+    const menuTitleCon = document.getElementById("title-con");
+    if (menuTitleCon.classList.contains("pop")) {
+        menuTitleCon.classList.remove("pop");
+    }
     menuTitleCon.offsetWidth = menuTitleCon.offsetWidth;
     menuTitleCon.classList.add("pop");
 }
 
-
 function bouncLogo() {
-    let logo = document.getElementById("logo");
-        if (logo.classList.contains("bounce")) {
-            logo.classList.remove("bounce");
-        }
+    const logo = document.getElementById("logo");
+    if (logo.classList.contains("bounce")) {
+        logo.classList.remove("bounce");
+    }
     logo.offsetWidth = logo.offsetWidth;
     logo.classList.add("bounce");
 }
 
-let getMenuBtn = document.getElementById("getMenuBtn");
+////// press button - do random menu
+const getMenuBtn = document.getElementById("getMenuBtn");
 let randCount = 0;
 getMenuBtn.addEventListener("click", async () => {
     await setMenuTx();
@@ -85,11 +219,38 @@ getMenuBtn.addEventListener("click", async () => {
     randCount++;
 
     // subtitle show
-    let subtitle = document.getElementById("subtitle");
+    const subtitle = document.getElementById("subtitle");
     if (randCount >= 10 && randCount <= 30) {
         subtitle.innerHTML = `ไม่ถูกใจ ${randCount} ครั้งแล้ว`;
     }
     else if (randCount > 30) {
         subtitle.innerHTML = `${randCount} รอบแล้ว.. จะได้กินมั้ยนะ?`;
     }
+});
+
+//////////////////////// setting menu ////////////////////////
+
+// show setting menu
+const settingBtn = document.getElementById("settingBtn");
+const settingMenu = document.getElementById("setting-container");
+settingBtn.addEventListener("click", () => {
+    settingMenu.classList.toggle("hidden");
+    if (settingMenu.classList.contains("slide-out")) {
+        settingMenu.classList.remove("slide-out");
+    }
+    settingMenu.offsetWidth = settingMenu.offsetWidth;
+    settingMenu.classList.add("slide-in");
+});
+
+// close setting menu
+const closeBtn = document.getElementById("closeSettingBtn");
+closeBtn.addEventListener("click", () => {
+    if (settingMenu.classList.contains("slide-in")) {
+        settingMenu.classList.remove("slide-in");
+    }
+    settingMenu.offsetWidth = settingMenu.offsetWidth;
+    settingMenu.classList.add("slide-out");
+    setTimeout(() => {
+        settingMenu.classList.toggle("hidden");
+    }, 400);
 });
